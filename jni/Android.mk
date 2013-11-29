e1cbeaba12ba72754dccbdd544ff3e52a10aeeab
Oliver Reiche <oliver.reiche@cs.fau.de>
2013-11-29 10:19:21 +0100

    Makefile: Automatically find HIPAcc includes

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,22 +4,29 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := hipacc_filters
 
+HIPACC_INCLUDES := $(subst hipacc,..,$(shell which hipacc))/include \
+                   $(subst hipacc,..,$(shell which hipacc))/include/dsl
+
 LOCAL_C_INCLUDES := $(SYSROOT_LINK)/usr/include/rs/cpp \
                     $(SYSROOT_LINK)/usr/include/rs \
                     obj/local/armeabi/objs/$(LOCAL_MODULE)/hipacc_gen \
-                    /scratch-local/usr/include
-										#$(HIPACC_INCLUDE)
+                    $(HIPACC_INCLUDES)
 
 LOCAL_CPPFLAGS += -DRS_TARGET_API=19 -DSIZE_X=5 -DSIZE_Y=5
 
-HIPACC_RESULT := $(shell hipacc -emit-renderscript -std=c++11 \
-                             -I$(shell clang -print-file-name=include) \
-                             -I$(shell llvm-config --includedir) \
-                             -I$(shell llvm-config --includedir)/c++/v1 \
-                             -I/usr/include \
-                             -I/scratch-local/usr/include/dsl \
-                             $(LOCAL_CPPFLAGS) -DHIPACC \
-                             jni/hipacc_src/blur.cpp -o jni/hipacc_gen/blur.cc)
+ifeq ($(CLEAN),true)
+  HIPACC_RESULT := $(shell rm $(LOCAL_PATH)/hipacc_gen/*)
+else
+  HIPACC_RESULT := $(shell cd $(LOCAL_PATH)/hipacc_gen; \
+                           hipacc -emit-renderscript -std=c++11 \
+                               -I/usr/include \
+                               -I$(shell clang -print-file-name=include) \
+                               -I$(shell llvm-config --includedir) \
+                               -I$(shell llvm-config --includedir)/c++/v1 \
+                               $(addprefix -I,$(HIPACC_INCLUDES)) \
+                               $(LOCAL_CPPFLAGS) -DHIPACC \
+                               ../hipacc_src/blur.cpp -o blur.cc)
+endif
 
 LOCAL_SRC_FILES := hipacc_filters.cpp \
                    $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/hipacc_gen/*.cc)) \
@@ -31,8 +38,7 @@ LOCAL_LDLIBS := -llog -ljnigraphics \
                 -l$(SYSROOT_LINK)/usr/lib/rs/libRScpp_static.a
 
 LOCAL_RENDERSCRIPT_FLAGS := -allow-rs-prefix -target-api 19 \
-                            -I/scratch-local/usr/include
-                            #-I$(HIPACC_INCLUDE)
+                            $(addprefix -I,$(HIPACC_INCLUDES))
 
 LOCAL_ARM_MODE := arm
 
