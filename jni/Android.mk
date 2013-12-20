316b08343853c733d32a6158f52b9adb340cd2f6
Oliver Reiche <oliver.reiche@cs.fau.de>
2013-12-20 13:46:57 +0100

    Refined Makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,16 +10,48 @@ LOCAL_LDLIBS := -llog -ljnigraphics
 
 LOCAL_ARM_MODE := arm
 
+
+################################################################################
 # Configure HIPAcc
+################################################################################
 HIPACC_RS_VERSION := 19
 HIPACC_SRC_PATH := hipacc_src
 HIPACC_GEN_PATH := hipacc_gen
-HIPACC_FLAGS := -rs-package org.hipacc.demo
 #HIPACC_INCLUDES := ...
 HIPACC_SRC_FILES := $(subst $(LOCAL_PATH)/$(HIPACC_SRC_PATH)/,,\
                             $(wildcard $(LOCAL_PATH)/$(HIPACC_SRC_PATH)/*.cpp))
 
-# Run HIPAcc
+
+################################################################################
+# Run HIPAcc for Renderscript
+################################################################################
+HIPACC_FLAGS := -emit-renderscript -rs-package org.hipacc.demo
+HIPACC_SRC_PREFIX := rs
 include $(LOCAL_PATH)/HIPAcc.mk
+
+
+################################################################################
+# Run HIPAcc for Filterscript
+################################################################################
+HIPACC_FLAGS := -emit-filterscript -rs-package org.hipacc.demo
+HIPACC_SRC_PREFIX := fs
+include $(LOCAL_PATH)/HIPAcc.mk
+
+
+################################################################################
+# Append generated sources
+################################################################################
+LOCAL_SRC_FILES += $(subst $(LOCAL_PATH)/,, \
+                           $(wildcard $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*.cpp)) \
+                   $(subst $(LOCAL_PATH)/,, \
+                           $(wildcard $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*.rs)) \
+                   $(subst $(LOCAL_PATH)/,, \
+                           $(wildcard $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*.fs))
+
+# Prepend define FILTERSCRIPT in generated Filterscript sources
+$(foreach SRC,$(HIPACC_SRC_FILES), \
+	$(shell sed -i "1i#define FILTERSCRIPT" \
+		$(LOCAL_PATH)/$(HIPACC_GEN_PATH)/fs$(SRC));)
+
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/HIPAcc.mk
+++ jni/HIPAcc.mk
@@ -1,7 +1,7 @@
+ifneq ($(HIPACC_SETUP_COMPLETE),1)
 ################################################################################
 # Setup local C/C++ to use Renderscript from NDK
 ################################################################################
-
 LOCAL_C_INCLUDES += $(SYSROOT_LINK)/usr/include/rs/cpp \
                     $(SYSROOT_LINK)/usr/include/rs
 LOCAL_LDLIBS += -l$(SYSROOT_LINK)/usr/lib/rs/libcutils.so \
@@ -9,9 +9,8 @@ LOCAL_LDLIBS += -l$(SYSROOT_LINK)/usr/lib/rs/libcutils.so \
 
 
 ################################################################################
-# Setup and run HIPAcc
+# Setup HIPAcc
 ################################################################################
-
 # Convert relative source path to absolute path
 HIPACC_SRC_PATH := $(shell pwd)/$(LOCAL_PATH)/$(HIPACC_SRC_PATH)
 
@@ -19,56 +18,39 @@ HIPACC_SRC_PATH := $(shell pwd)/$(LOCAL_PATH)/$(HIPACC_SRC_PATH)
 HIPACC_INCLUDES += $(subst hipacc,..,$(shell which hipacc))/include \
                    $(subst hipacc,..,$(shell which hipacc))/include/dsl
 
-ifeq ($(C),1)
-  # Clean generated files
-  HIPACC_CLEAN_RESULT := $(shell rm $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*)
-else
-  # Call HIPAcc to generate Renderscript sources
-  $(foreach SRC,$(HIPACC_SRC_FILES), \
-  	HIPACC_RS_RESULT := $(shell cd $(LOCAL_PATH)/$(HIPACC_GEN_PATH); \
-                                hipacc -emit-renderscript \
-                                    -std=c++11 \
-                                    $(HIPACC_FLAGS) \
-                                    -I/usr/include \
-                                    -I$(shell clang -print-file-name=include) \
-                                    -I$(shell llvm-config --includedir) \
-                                    -I$(shell llvm-config --includedir)/c++/v1 \
-                                    $(addprefix -I,$(HIPACC_INCLUDES)) \
-                                    $(LOCAL_CPPFLAGS) -DHIPACC \
-                                    $(HIPACC_SRC_PATH)/$(SRC) \
-                                    -o rs$(SRC));)
-
-  # Call HIPAcc to generate Filterscript sources
-  $(foreach SRC,$(HIPACC_SRC_FILES), \
-  	HIPACC_FS_RESULT := $(shell cd $(LOCAL_PATH)/$(HIPACC_GEN_PATH); \
-                                hipacc -emit-filterscript \
-                                    -std=c++11 \
-                                    $(HIPACC_FLAGS) \
-                                    -I/usr/include \
-                                    -I$(shell clang -print-file-name=include) \
-                                    -I$(shell llvm-config --includedir) \
-                                    -I$(shell llvm-config --includedir)/c++/v1 \
-                                    $(addprefix -I,$(HIPACC_INCLUDES)) \
-                                    $(LOCAL_CPPFLAGS) -DHIPACC \
-                                    $(HIPACC_SRC_PATH)/$(SRC) \
-                                    -o fs$(SRC); \
-                                sed -i "1i#define FILTERSCRIPT" fs$(SRC));)
-endif
-
 
 ################################################################################
-# Setup local C/C++ to use HIPAcc specific flags and compile generated files
+# Setup HIPAcc specific flags and sources to local C/C++
 ################################################################################
-
 LOCAL_CPPFLAGS += -DRS_TARGET_API=$(HIPACC_RS_VERSION) -DEXCLUDE_IMPL
 LOCAL_RENDERSCRIPT_FLAGS += -allow-rs-prefix -target-api $(HIPACC_RS_VERSION) \
                             $(addprefix -I,$(HIPACC_INCLUDES))
 LOCAL_C_INCLUDES += $(HIPACC_INCLUDES) \
                     obj/local/armeabi/objs/$(LOCAL_MODULE)/$(HIPACC_GEN_PATH)
-LOCAL_SRC_FILES += hipacc_runtime.cpp \
-                   $(subst $(LOCAL_PATH)/,, \
-                           $(wildcard $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*.cpp)) \
-                   $(subst $(LOCAL_PATH)/,, \
-                           $(wildcard $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*.rs)) \
-                   $(subst $(LOCAL_PATH)/,, \
-                           $(wildcard $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*.fs))
+LOCAL_SRC_FILES += hipacc_runtime.cpp
+
+
+# Mark setup complete
+HIPACC_SETUP_COMPLETE := 1
+endif
+
+
+################################################################################
+# Run HIPAcc
+################################################################################
+ifeq ($(HIPACC_CLEAN),1)
+# Clean generated files
+$(shell rm $(LOCAL_PATH)/$(HIPACC_GEN_PATH)/*)
+else
+# Call HIPAcc to generate sources
+$(foreach SRC,$(HIPACC_SRC_FILES), \
+  	$(shell cd $(LOCAL_PATH)/$(HIPACC_GEN_PATH); \
+            hipacc $(HIPACC_FLAGS) -std=c++11 \
+                -I/usr/include \
+                -I$(shell clang -print-file-name=include) \
+                -I$(shell llvm-config --includedir) \
+                -I$(shell llvm-config --includedir)/c++/v1 \
+                $(addprefix -I,$(HIPACC_INCLUDES)) \
+                $(LOCAL_CPPFLAGS) -DHIPACC \
+                $(HIPACC_SRC_PATH)/$(SRC) -o $(HIPACC_SRC_PREFIX)$(SRC));)
+endif
